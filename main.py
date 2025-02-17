46acf62ab43ee28592854b908c2e2dd0b7249a4c
Happy Rajput <happyrajput@Happys-MacBook-Air.local>
2025-02-18 03:11:07 +0530

    Completed the data ingestion process

---
+++ main.py
@@ -0,0 +1,22 @@
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+from networksecurity.entity.config_entity import DataIngestionConfig
+from networksecurity.entity.config_entity import TrainingPipelineConfig
+
+if __name__=='__main__':
+    try:
+        training_pipeline_config=TrainingPipelineConfig()
+
+        dataingestionconfig=DataIngestionConfig(training_pipeline_config)
+        data_ingestion=DataIngestion(dataingestionconfig)
+
+        logging.info("Initate the data ingestion")
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        print(dataingestionartifact)
+
+    except Exception as e:
+        raise NetworkSecurityException(e,sys)
+    
+
+
