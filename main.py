568a6d8b9fa69035046ac8d0a28b4e833b18489c
Happy Rajput <happyrajput@Happys-MacBook-Air.local>
2025-02-19 14:30:58 +0530

    Data Transformation Completed

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 import sys
 
 if __name__=='__main__':
@@ -20,7 +21,16 @@ if __name__=='__main__':
         logging.info("Initate the Data Validation")
         data_validation_artifact=data_validation.initiate_data_validation()
         logging.info("Data Validation Completed")
-        print(data_validation_artifact)
+        # print(data_validation_artifact)
+
+        data_transformation_config=DataTransformationConfig(training_pipeline_config)
+        logging.info("Data Transformation Initated")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("Data Transformation Completed")
+
+
 
 
 
